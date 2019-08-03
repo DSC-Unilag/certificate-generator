9a3a23dd015e85b0505e8bf7f9c952dd2b7e3af6
LordGhostX <solomonghost9@gmail.com>
2019-08-03 00:26:22 +0000

    added custom fonting

--- generator.py
+++ generator.py
@@ -5,16 +5,19 @@ from PIL import ImageDraw
 import os
 
 def generator(template, data, locations, font=None, font_size=None, font_color=(0, 0, 0)):
+    # font
+    font = ImageFont.truetype("Eutemia.ttf", 44)
+
     # open the template file
     img = Image.open(template)
     draw = ImageDraw.Draw(img)
 
     for i in range(len(data)):
-        draw.text((locations[i][0], locations[i][1]), data[i], (0, 0, 0))
+        draw.text((locations[i][0], locations[i][1]), data[i], font_color, font=font)
 
     # Check if output destination exists
     if not os.path.exists("certificates/"):
-        os.system("md certificates")
+        os.system("mkdir certificates")
 
     name = data[0]
     outfilename = "certificates/" + "-".join(name.split()) + "-certificate." + template.split(".")[-1]
