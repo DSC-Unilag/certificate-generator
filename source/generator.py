416e60de55b3caa3fd08381c21c306609ff00433
LordGhostX <solomonghost9@gmail.com>
2019-08-31 02:30:19 +0000

    added generator.py

--- source/generator.py
+++ source/generator.py
@@ -1,27 +1,50 @@
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 from os import mkdir
+from program import join
+from random import randint
 
 def get_dim(path):
     img = Image.open(path)
     return img.width, img.height
 
-def generator(template, data, locations, font=None, font_size=None, font_color=(0, 0, 0)):
-    # font
-    font = ImageFont.truetype("Eutemia.ttf", 44)
+def gen_img(img_data, img_points, id_, extension=None, allow_random=True, center_width=False, center_height=False):
+    with open(join("UI/temp", "dsc-cert-gen.json")) as f:
+        j = load(f)
+    try:
+        mkdir(j["event_name"])
+    except:
+        pass
 
-    # open the template file
-    img = Image.open(template)
-    draw = ImageDraw.Draw(img)
+    for data in img_data:
+        img = Image.open(join("UI", j["template"]))
+        draw = ImageDraw.Draw(img)
+        index = 0
+        for point in img_points:
+            font = ImageFont.truetype(point["font_name"], point["font_size"])
+            x, y = point["x"], point["y"]
+            w, h = draw.textsize(data[index], font=font)
+            W, H = img.size
+            if center_width:
+                x = (W - w) / 2
+            if center_height:
+                y = (H - h) / 2
+            draw.text((x, y), data[index], fill=point["font_color"], font=font)
+            index += 1
 
-    for i in range(len(data)):
-        draw.text((locations[i][0], locations[i][1]), data[i], font_color, font=font)
+        if extension == None:
+            extension = j["template"].split(".")[-1]
 
-    # Check if output destination exists
-    if not os.path.exists("certificates/"):
-        os.system("mkdir certificates")
+        if extension.lower() == "png":
+            if len(img.split()) == 4:
+                background = Image.new("RGB", img.size, (255, 255, 255))
+                background.paste(img, mask=img.split()[3])
+                img = background
+                del background
 
-    name = data[0]
-    outfilename = "certificates/" + "-".join(name.split()) + "-certificate." + template.split(".")[-1]
-    img.save(outfilename)
+        if allow_random:
+            unique_name = data[id_] + "-" + str(rand(1, 10000))
+        else:
+            unique_name = data[id_]
+
+        outfilename = join(j["event_name"], unique_name + "." + extension)
+        img.save(outfilename)
